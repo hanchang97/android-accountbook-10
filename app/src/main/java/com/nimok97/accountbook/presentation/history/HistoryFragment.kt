983bdc9b6bb8fe21b33311ab230b68eb5a97a7d8
한창희 <nimok97@gmail.com>
2022-07-27 21:28:00 +0900

    [ADD] - 탭별 프래그먼트 생성

---
+++ app/src/main/java/com/nimok97/accountbook/presentation/history/HistoryFragment.kt
@@ -0,0 +1,30 @@
+package com.nimok97.accountbook.presentation.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.nimok97.accountbook.R
+import com.nimok97.accountbook.databinding.FragmentHistoryBinding
+
+class HistoryFragment: Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_history, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/nimok97/accountbook/presentation/statistics/StatisticsFragment.kt
@@ -0,0 +1,30 @@
+package com.nimok97.accountbook.presentation.statistics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.nimok97.accountbook.R
+import com.nimok97.accountbook.databinding.FragmentStatisticsBinding
+
+class StatisticsFragment: Fragment() {
+
+    private lateinit var binding: FragmentStatisticsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_statistics, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
