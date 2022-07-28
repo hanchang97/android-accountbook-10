390944e1e4578090521edab6bd4358d2ef0e0e30
한창희 <nimok97@gmail.com>
2022-07-28 09:49:20 +0900

    [ADD] - ManageHistoryFragment 추가 수입, 지출, 수정 담당 예정

--- app/src/main/java/com/nimok97/accountbook/presentation/history/manage/ManageHistoryFragment.kt
+++ app/src/main/java/com/nimok97/accountbook/presentation/history/manage/ManageHistoryFragment.kt
@@ -1,4 +1,4 @@
-package com.nimok97.accountbook.presentation.history
+package com.nimok97.accountbook.presentation.history.manage
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,33 +8,26 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.nimok97.accountbook.R
 import com.nimok97.accountbook.common.printLog
-import com.nimok97.accountbook.databinding.FragmentHistoryBinding
+import com.nimok97.accountbook.databinding.FragmentManageHistoryBinding
 
-class HistoryFragment: Fragment() {
+class ManageHistoryFragment: Fragment() {
 
-    private lateinit var binding: FragmentHistoryBinding
+    private lateinit var binding: FragmentManageHistoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        printLog("HistoryFragment / onCreateView")
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_history, container, false)
+        printLog("ManageHistoryFragment / onCreateView")
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_manage_history, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initView()
-    }
-
-    private fun initView() {
-        setFab()
-    }
-
-    private fun setFab(){
 
     }
+
 }
